510431181d99e32d41bc2b5eeb3e44e410a3bdbe
ahavamorse <ahavamorse@icloud.com>
2021-07-31 15:05:29 -0700

    Created Card model and CareImages enum of images

---
+++ MemoryGame/MemoryGame/Card.swift
@@ -0,0 +1,14 @@
+//
+//  Card.swift
+//  MemoryGame
+//
+//  Created by HapiDani on 7/31/21.
+//  Copyright © 2021 ahavamorse. All rights reserved.
+//
+
+import UIKit
+
+struct Card {
+    var isFlipped: Bool = false
+    var image: UIImage
+}
---
+++ MemoryGame/MemoryGame/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  MemoryGame
+//
+//  Created by HapiDani on 7/31/21.
+//  Copyright © 2021 ahavamorse. All rights reserved.
+//
+
+import UIKit
+
+enum CardImages {
+    static let bat = UIImage(named: "memoryBatCardFront")!
+    static let cat = UIImage(named: "memoryCatCardFront")!
+    static let cow = UIImage(named: "memoryCowCardFront")!
+    static let dragon = UIImage(named: "memoryDragonFront")!
+    static let man = UIImage(named: "memoryGarbageManCardFront")!
+    static let dog = UIImage(named: "memoryGhostDogCardFront")!
+    static let hen = UIImage(named: "memoryHenCardFront")!
+    static let horse = UIImage(named: "memoryHorseCardFront")!
+    static let pig = UIImage(named: "memoryPigCardFront")!
+    static let spider = UIImage(named: "memorySpiderCardFront")!
+}
