ac5b2c1e072c3b8812f07700967a89b42b8997d8
ahavamorse <ahavamorse@icloud.com>
2021-07-31 11:31:46 -0700

    Implemented configureUI function for LobbyViewController and adjusted button appearance

--- MemoryGame/MemoryGame/LobbyViewController.swift
+++ MemoryGame/MemoryGame/LobbyViewController.swift
@@ -11,14 +11,15 @@ import UIKit
 class LobbyViewController: UIViewController {
     
     let stackView = UIStackView()
-    let buttons = Array(repeating: UIButton(), count: 4)
-    let sizeStrings = ["2 x 5", "3 x 4", "4 x 4", "4 x 5"]
+    let buttons = [UIButton(frame: .zero), UIButton(frame: .zero), UIButton(frame: .zero), UIButton(frame: .zero)]
+    let sizeStrings = ["2 Cards x 5 Cards", "3 Cards x 4 Cards", "4 Cards x 4 Cards", "4 Cards x 5 Cards"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewController()
         configureStackView()
         configureButtons()
+        configureUI()
     }
     
     private func configureViewController() {
@@ -30,7 +31,8 @@ class LobbyViewController: UIViewController {
     private func configureStackView() {
         stackView.axis = .vertical
         stackView.alignment = .center
-        stackView.distribution = .equalSpacing
+        stackView.distribution = .fillEqually
+        stackView.spacing = 20
         
         for button in buttons {
             stackView.addArrangedSubview(button)
@@ -40,13 +42,38 @@ class LobbyViewController: UIViewController {
     private func configureButtons() {
         for index in 0...3 {
             let button = buttons[index]
+            button.backgroundColor = .secondaryLabel
+            button.layer.cornerRadius = 15
+            button.titleLabel?.font = .preferredFont(forTextStyle: .largeTitle)
+            
             button.tag = index
-            button.titleLabel?.text = sizeStrings[index]
+            button.setTitle(sizeStrings[index], for: .normal)
             button.addTarget(self, action: #selector(startGame), for: .touchUpInside)
         }
     }
     
     @objc private func startGame(sender: UIButton) {
         
+    }
+    
+    private func configureUI() {
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 150),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -150)
+            
+//            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+//            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        for button in buttons {
+            NSLayoutConstraint.activate([
+                button.widthAnchor.constraint(equalTo: stackView.widthAnchor)
+            ])
+        }
     }
 }
