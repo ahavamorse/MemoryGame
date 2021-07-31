d473420dba79a5cfbdc7e285d8c95a2479d5bba3
ahavamorse <ahavamorse@icloud.com>
2021-07-31 12:20:40 -0700

    Implemented configure NavigationBar function for GamePlayViewController

--- MemoryGame/MemoryGame/GamePlayViewController.swift
+++ MemoryGame/MemoryGame/GamePlayViewController.swift
@@ -12,13 +12,25 @@ class GamePlayViewController: UIViewController {
     
     var gridWidth: Int = 0
     var gridHeight: Int = 0
+    var gridSizeString: String = ""
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureNavigationBar()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewController()
+        configureNavigationBar()
     }
     
     private func configureViewController() {
         view.backgroundColor = .systemBackground
+        title = gridSizeString
+    }
+    
+    private func configureNavigationBar() {
+        navigationController?.navigationBar.isHidden = false
     }
 }
