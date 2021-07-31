8f8576579740fc5ae4ebeb6dbfeb48f1217fbe59
ahavamorse <ahavamorse@icloud.com>
2021-07-31 12:08:33 -0700

    Created GamePlayViewController and variables

---
+++ MemoryGame/MemoryGame/GamePlayViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GamePlayViewController.swift
+//  MemoryGame
+//
+//  Created by HapiDani on 7/31/21.
+//  Copyright © 2021 ahavamorse. All rights reserved.
+//
+
+import UIKit
+
+class GamePlayViewController: UIViewController {
+    
+    var gridWidth: Int = 0
+    var gridHeight: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
