083a32fdbe261c6ed17959b49994fd43e1294b71
ahavamorse <ahavamorse@icloud.com>
2021-07-31 11:40:42 -0700

    Added titleLabel and adjusted UI

--- MemoryGame/MemoryGame/LobbyViewController.swift
+++ MemoryGame/MemoryGame/LobbyViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class LobbyViewController: UIViewController {
     
+    let titleLabel = UILabel()
     let stackView = UIStackView()
     let buttons = [UIButton(frame: .zero), UIButton(frame: .zero), UIButton(frame: .zero), UIButton(frame: .zero)]
     let sizeStrings = ["2 Cards x 5 Cards", "3 Cards x 4 Cards", "4 Cards x 4 Cards", "4 Cards x 5 Cards"]
@@ -17,15 +18,21 @@ class LobbyViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewController()
+        configureTitleLabel()
         configureStackView()
         configureButtons()
         configureUI()
     }
     
     private func configureViewController() {
-        view.backgroundColor = .systemBackground
-        navigationController?.navigationBar.prefersLargeTitles = true
-        title = "Memory Game"
+        view.backgroundColor = .systemTeal
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    private func configureTitleLabel() {
+        titleLabel.text = "Memory Game"
+        titleLabel.font = .preferredFont(forTextStyle: .largeTitle)
+        titleLabel.textColor = .systemBackground
     }
     
     private func configureStackView() {
@@ -42,7 +49,7 @@ class LobbyViewController: UIViewController {
     private func configureButtons() {
         for index in 0...3 {
             let button = buttons[index]
-            button.backgroundColor = .secondaryLabel
+            button.backgroundColor = .tertiaryLabel
             button.layer.cornerRadius = 15
             button.titleLabel?.font = .preferredFont(forTextStyle: .largeTitle)
             
@@ -57,17 +64,19 @@ class LobbyViewController: UIViewController {
     }
     
     private func configureUI() {
+        view.addSubview(titleLabel)
         view.addSubview(stackView)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
         stackView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 150),
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 200),
             stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
             stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
-            stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -150)
-            
-//            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-//            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100)
         ])
         
         for button in buttons {
